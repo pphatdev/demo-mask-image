262512a2a096a67797be8c63cced296384e7816c
pphatdev <leatsophat02@gmail.com>
2025-05-14 21:57:48 +0700

    Enhance RootLayout component: add Providers wrapper for improved context management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Kantumruy_Pro, Poppins } from "next/font/google";
+import { Providers } from "./providers";
 import "./globals.css";
 
 const kantumruyPro = Kantumruy_Pro({
@@ -59,9 +60,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en">
-            <body className={`${kantumruyPro.variable} ${poppins.variable} antialiased font-sans`} >
-                {children}
+        <html lang="en" suppressHydrationWarning>
+            <body className={`${kantumruyPro.variable} ${poppins.variable} antialiased font-sans`}>
+                <Providers>{children}</Providers>
             </body>
         </html>
     );
