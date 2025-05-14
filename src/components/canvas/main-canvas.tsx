22e15373b9cd40be88570a400a8495966bef23a4
pphatdev <pphat.kits@gmail.com>
2025-05-14 17:22:27 +0700

    Refactor Canvas component layout and styles
    - Changed the outer div to use a background color instead of relative positioning.
    - Removed unnecessary background and border styles from the canvas element.

--- src/components/canvas/main-canvas.tsx
+++ src/components/canvas/main-canvas.tsx
@@ -76,10 +76,10 @@ export const Canvas = ({ selectedMask, selectedImage }: CanvasProps) => {
     }, [selectedMask, selectedImage]);
 
     return (
-        <div className='relative w-full h-full'>
+        <div className='bg-background'>
             { loading && <div className="absolute inset-0 flex items-center justify-center bg-white/50 z-10"> <Riple color="#32cd32" size="large" /> </div> }
             <SaveButton onClick={onSave} />
-            <canvas ref={canvasRef} className="h-full z-50 aspect-square w-full bg-center border border-foreground/10 rounded-4xl" />
+            <canvas ref={canvasRef} className="h-full z-50 aspect-square w-full bg-center" />
         </div>
     );
 };
